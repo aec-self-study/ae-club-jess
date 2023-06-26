f5567fb21262db4715b0f2826b5ff21093f37ca7
Jess Streeter <jstreeter.rutgers@gmail.com>
2023-06-26 16:10:24 +0000

    dbt101

--- dbt_jess/models/customers.sql
+++ dbt_jess/models/customers.sql
@@ -1,3 +1,7 @@
+{{ config(
+    materialized='table'
+ ) }}
+
 select min(o.created_at) as first_order_at
 , count(*) as number_orders
 , c.name 
@@ -5,5 +9,4 @@ from `analytics-engineers-club.coffee_shop.customers` as c
 left join `analytics-engineers-club.coffee_shop.orders` as o 
   on o.customer_id = c.id
 group by c.name
-order by first_order_at 
-limit 5;
+
